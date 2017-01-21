c2447ed6d68acba17dd84b41720236d68f963208
Arnold <arnybarnie@gmail.com>
2017-01-21 19:07:27 +0800

    Product Page Initial

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about_us
+  end
+
+  def articles
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
-Rails.application.routes.draw do
-  resources :car_seat_items
+Rails.application.routes.draw do 
+  root 'pages#home'
+ resources :car_seat_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ get 'pages/about_us'
+
+  get 'pages/articles'
+
+  
+
 end
